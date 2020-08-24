82448280bc8bf1af1630c59f27e23a0981bd86c2
legionpunisher <mbuguamoses656@gmail.com>
2020-08-24 06:20:37 +0300

    added id and name property

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+quotes:Quote[]=[
+  {id:1,name:'Happiness depends upon ourselves'},
+  {id:2,name:'Everything you can imagine is real'},
+  {id:3,name:'Sipimplicity is the ultimate sophistication'},
+  {id:4,name:'What we think we become',},
+];
   constructor() { }
 
   ngOnInit(): void {
