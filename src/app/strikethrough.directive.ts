9bed2a74c41be05c0dd4f841f38a02765e03f6d9
legionpunisher <mbuguamoses656@gmail.com>
2020-08-24 10:14:30 +0300

    added a reference injector element Ref to the host Dom

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,10 +1,9 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
-
-  constructor() { }
-
+  constructor(private elem:ElementRef){}
+  
 }
